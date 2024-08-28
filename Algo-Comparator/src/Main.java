34a839ba0eca418438163353af2d00054a367785
Thomas U. <Info@ThomasU.dev>
2024-08-29 00:55:06 +0200

    Add Random Generate String & Chance Output

--- Algo-Comparator/src/Main.java
+++ Algo-Comparator/src/Main.java
@@ -1,23 +1,52 @@
 import Algorithm.BubbleSort;
 import Algorithm.SortAlgorithm;
 
+import java.security.SecureRandom;
+
+import static java.lang.System.*;
+
 public class Main {
+
+    private static final String LOWERCASE = "abcdefghijklmnopqrstuvwxyz";
+    private static final String UPPERCASE = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private static final String DIGITS = "0123456789";
+    private static final String SPECIAL_CHARACTERS = "!@#$%^&*()-_=+[]{}|;:'\",.<>?/~`";
+    private static final String UMLAUTS = "äöüÄÖÜ";
+
+    private static final String ALL_CHARACTERS = LOWERCASE + UPPERCASE + DIGITS + SPECIAL_CHARACTERS + UMLAUTS;
+
+    private static final SecureRandom RANDOM = new SecureRandom();
+
     public static void main(String[] args) {
-        var input = "a1B2c3b3A4D5e6F7g8H9t23ffvaexddv54ä4pvü423i0J!@#df3f22fwfsfsfsfsf3fwfsfs<fs<fs<fw3fsfsfsfsefs<f34232e32f3fbgb##sä29g33f";
+        String input = generateRandomString(100);
+        out.println("Generated Random String: " + input);
+        out.println();
 
         SortAlgorithm[] algorithms = {
                 new BubbleSort(),
         };
 
         for (SortAlgorithm algorithm : algorithms) {
-            var startTime = System.currentTimeMillis();
+            var startTimeMillis = currentTimeMillis();
+            var startTimeNanos = nanoTime();
             String sortedOutput = algorithm.sort(input);
-            var endTime = System.currentTimeMillis();
-            var duration = endTime - startTime;
+            var endTimeMillis = currentTimeMillis();
+            var endTimeNanos = nanoTime();
+            var durationMillis = endTimeMillis - startTimeMillis;
+            var durationNanos = endTimeNanos - startTimeNanos;
 
-            System.out.println(algorithm.getClass().getSimpleName() + " took " + duration + " ns");
-            System.out.println("Sorted Output: " + sortedOutput);
-            System.out.println();
+            out.println(algorithm.getClass().getSimpleName() + " took " + durationMillis + " ms / " + durationNanos + " ns");
+            out.println("Sorted Output: " + sortedOutput);
+            out.println();
         }
+    }
+
+    private static String generateRandomString(int length) {
+        var result = new StringBuilder(length);
+        for (int i = 0; i < length; i++) {
+            var index = RANDOM.nextInt(ALL_CHARACTERS.length());
+            result.append(ALL_CHARACTERS.charAt(index));
+        }
+        return result.toString();
     }
 }
