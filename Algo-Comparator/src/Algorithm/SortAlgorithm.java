80cae031980fe6ef2b199ef2582847af2f50619a
Thomas U. <Info@ThomasU.dev>
2024-08-29 00:06:52 +0200

    Add input String & Interface

---
+++ Algo-Comparator/src/Algorithm/SortAlgorithm.java
@@ -0,0 +1,5 @@
+package Algorithm;
+
+public interface SortAlgorithm {
+    String sort(String input);
+}
--- Algo-Comparator/src/Main.java
+++ Algo-Comparator/src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main() {
+        var input = "a1B2c3b3A4D5e6F7g8H9t2354ä4pvü423i0J!@#";
     }
 }
